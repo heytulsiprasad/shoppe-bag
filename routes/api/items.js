750610fa747a0fba41cf02c0e432c363b8d9333b
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-07-10 20:43:01 +0530

    connect with backend for crud requests

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,44 +1,41 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // Item Model
-const Item = require("../../models/Items");
-const { json } = require("express");
-const { remove } = require("../../models/Items");
-
-const fluffy = new Item({ name: "fluffy" });
-fluffy;
+const Item = require('../../models/Items');
+const { json } = require('express');
+const { remove } = require('../../models/Items');
 
 // @route GET api/items
 // @desc Get All Items
 // @access Public
 
-router.get("/", (req, res) => {
-	Item.find()
-		.sort({ date: -1 }) // descending order
-		.then((items) => res.json(items));
+router.get('/', (req, res) => {
+  Item.find()
+    .sort({ date: -1 }) // descending order
+    .then((items) => res.json(items));
 });
 
 // @route POST api/items
 // @desc  Create an Item
 // @access Public
 
-router.post("/", (req, res) => {
-	const newItem = new Item({
-		name: req.body.name,
-	});
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+  });
 
-	newItem.save().then((item) => res.json(item));
+  newItem.save().then((item) => res.json(item));
 });
 
 // @route DELETE api/items/:id
 // @desc  Delete A Item
 // @access Public
 
-router.delete("/:id", (req, res) => {
-	Item.findById(req.params.id)
-		.then((item) => item.remove().then(() => res.json({ success: true })))
-		.catch((err) => res.status(404).json({ success: false }));
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then((item) => item.remove().then(() => res.json({ success: true })))
+    .catch((err) => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
