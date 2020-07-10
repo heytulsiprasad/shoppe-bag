3ea4b2dd56b954d8edea39edfac1c864eef52c98
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-07-10 19:34:27 +0530

    add items via redux

--- client/src/redux/reducers/itemReducer.js
+++ client/src/redux/reducers/itemReducer.js
@@ -11,10 +11,20 @@ const initialState = {
 };
 
 function itemReducer(state = initialState, action) {
-  switch (action) {
+  switch (action.type) {
     case GET_ITEMS:
       return {
         ...state,
+      };
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload),
+      };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items],
       };
     default:
       return state;
