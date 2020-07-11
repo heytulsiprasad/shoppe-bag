e2ae61f9920520e317bd2b8f06a1436b569c6204
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-07-11 17:02:40 +0530

    chain npm scripts using bash

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
