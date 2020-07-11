cc4cf72a2256397734856dfbd63dd6a210f89c4f
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-07-11 19:22:05 +0530

    version 2.0

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ app.use('/api/items', items);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use('/', express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
