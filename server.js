b64edc303101a43a042c4f30a5dc860ad9197949
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-07-11 13:03:30 +0530

    test production to heroku

--- server.js
+++ server.js
@@ -1,31 +1,47 @@
-const express = require("express");
-const mongoose = require("mongoose");
+require('dotenv').config();
 
-const items = require("./routes/api/items");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const items = require('./routes/api/items');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // DB config
-const db = require("./config/keys").mongoURI;
+const db =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_ATLAS_URI
+    : process.env.MONGODB_LOCAL_URI;
 
 // Connect to MongoDB
 mongoose.connect(db, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-mongoose.connection.on("error", (error) =>
-	console.log("Connection error", error)
+mongoose.connection.on('error', (error) =>
+  console.log('Connection error', error)
 );
 
-mongoose.connection.once("open", () =>
-	console.log("Local database is connected...")
+mongoose.connection.once('open', () =>
+  console.log('Local database is connected...')
 );
 
 // Use Routes
-app.use("/api/items", items);
+app.use('/api/items', items);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
